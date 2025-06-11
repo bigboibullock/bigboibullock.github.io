cd389d6b2fa39e330a35c80df094ec1a49410d54
Balraj <balraj.johal@phntms.com>
2025-06-12 00:18:23 +0100

    Setup work categories

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,8 +2,30 @@ import { defineCollection } from "astro:content";
 
 import { glob } from "astro/loaders";
 
-const work = defineCollection({
+const ALL_WORK = defineCollection({
   loader: glob({ pattern: "work-content/**/*.md" }),
 });
 
-export const collections = { work };
+const ANIMATION_WORK = defineCollection({
+  loader: glob({ pattern: "work-content/animation/*.md" }),
+});
+
+const WEB_WORK = defineCollection({
+  loader: glob({ pattern: "work-content/web/*.md" }),
+});
+
+const VIDEO_WORK = defineCollection({
+  loader: glob({ pattern: "work-content/video/*.md" }),
+});
+
+const DIGITAL_WORK = defineCollection({
+  loader: glob({ pattern: "work-content/digital/*.md" }),
+});
+
+export const collections = {
+  work: ALL_WORK,
+  animation_work: ANIMATION_WORK,
+  web_work: WEB_WORK,
+  video_work: VIDEO_WORK,
+  digital_work: DIGITAL_WORK,
+};
